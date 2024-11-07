05ace304d7e0208c08978c9c223ec70a0d1f2ac6
TaukTauk <tauktauk51833@gmail.com>
2024-11-07 22:29:25 +0700

    react first project

---
+++ typescript/dict/test.js
@@ -0,0 +1,9 @@
+"use strict";
+let message = 'Hello, TypeScript!';
+console.log(message);
+let num = 30;
+console.log("hello");
+console.log(num);
+console.log('hello');
+console.log('rick');
+console.log('topp');
---
+++ typescript/src/test.ts
@@ -0,0 +1,14 @@
+let message: string = 'Hello, TypeScript!'; console.log(message);
+
+let num: number = 30
+
+console.log("hello")
+console.log(num)
+
+
+
+console.log('hello')
+
+console.log('rick')
+
+console.log('topp')
